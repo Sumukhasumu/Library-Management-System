711d5228cf7ae21d934f159e0a6434f6de7a0bbe
Sumukhasumu <93380756+Sumukhasumu@users.noreply.github.com>
2026-03-24 22:15:26 +0530

    Update WelcomeController.java

--- library-management/src/main/java/com/library/controller/WelcomeController.java
+++ library-management/src/main/java/com/library/controller/WelcomeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WelcomeController {
 
     @GetMapping("/")
+    @ResponseBody
     public String home() {
         return "login";  // OR "welcome" if you prefer
     }
